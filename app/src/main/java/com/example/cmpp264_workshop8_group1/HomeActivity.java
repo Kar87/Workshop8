ddb75fd030f5c745dc539bb1f6178ce72eda31d2
nicoderilo <nicoderilo@gmail.com>
2021-10-21 20:02:31 -0600

    Oct 21 8:01pm
    added 2 icons, GUI, Link to website

--- app/src/main/java/com/example/cmpp264_workshop8_group1/HomeActivity.java
+++ app/src/main/java/com/example/cmpp264_workshop8_group1/HomeActivity.java
@@ -3,13 +3,14 @@ package com.example.cmpp264_workshop8_group1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
     //reference to controls
-    ImageView ivProfile, ivBookings,ivPackages, ivContactUs;
+    ImageView ivProfile, ivBookings,ivPackages, ivContactUs,ivWebsite, ivUnknown;
 
     @Override
     //application starts
@@ -21,19 +22,33 @@ public class HomeActivity extends AppCompatActivity {
         ivBookings = findViewById(R.id.ivBookings);
         ivPackages = findViewById(R.id.ivPackages);
         ivContactUs = findViewById(R.id.ivContactUs);
+        ivWebsite = findViewById(R.id.ivWebsite);
 
+        Intent intent = getIntent();
+        //String mode = intent.getStringExtra("mode");
+        String user = intent.getStringExtra("user");
+        String pwd = intent.getStringExtra("password");
+
+        //Links to edit profile
         ivProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-                intent.putExtra("mode", "insert");
-                //LoginActivity.etUsername1.getText();
+
+                Intent intent = new Intent(HomeActivity.this, RegisterActivity.class);
+                intent.putExtra("user",user);
+                intent.putExtra("password",pwd);
+                intent.putExtra("mode", "update");
                 startActivity(intent);
-
-
-
-
             }
         });
+
+        ivWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.travelagencygroup2.website/"));
+                startActivity(browserIntent);
+            }
+        });
+
     }
 }
